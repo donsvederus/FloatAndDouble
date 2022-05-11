e0b8c180f1250b47c39bc0ae9c339779cce0f45b
Don Svederus <svederus@gmail.com>
2022-05-10 20:28:07 -0600

    0.030 Floating Point Precision and a Challenge

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -13,8 +13,22 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.0 / 3.0; // dont need the d if using decimals
+
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        System.out.println("------ Challenge ------");
+
+        double kilograms = 0.45459237;
+        double numberOfPounds = 4.0;
+        double convertedToKilograms = numberOfPounds * kilograms;
+
+        System.out.println(numberOfPounds + " pounds = " + convertedToKilograms + " kilograms");
+
+
     }
 }
